517d29f77ca897b4d38e0b6f5337e0924b1c5a01
couch2coders <couch2coders@gmail.com>
2026-03-31 18:03:34 -0400

    Update approve_restaurant.py

--- Restaurants/code/approve_restaurant.py
+++ Restaurants/code/approve_restaurant.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-import os, sys
-sys.path.append(os.path.dirname(__file__))
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'Code'))
 from notion_helper import approve_restaurant_in_notion
 
 APPROVED_PLACE_ID = os.environ["APPROVED_PLACE_ID"]
